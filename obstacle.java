0f5f0ef60d15247e127bb41796b0b34b3f83adc0
kalebmarcel <kalebmarcel@users.noreply.github.com>
2015-06-08 16:03:57 +0700

    Create obstacle.java

---
+++ obstacle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Obstacle
+{
+	public int x1, y1, x2, y2, x3, y3;
+
+	public void SetobstaclePosition()
+	{
+	x1=300;
+	y1=500;
+	
+	x2=200;
+	y2=400;
+	
+	x3=600;
+	y3=300;
+	}
+}
