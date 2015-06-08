fb6dab5fe031667047bfde25a93f2cd132ab19a8
kalebmarcel <kalebmarcel@users.noreply.github.com>
2015-06-08 16:03:28 +0700

    Create fueltank.java

---
+++ fueltank.java
@@ -0,0 +1,11 @@
+public class FuelTank {
+	
+	public int amountOfFuel=100;
+	
+	public int getAmountOfFuel(){
+		return amountOfFuel;
+	}
+	public void decrementFuelTank(){
+			amountOfFuel=amountOfFuel - 1;
+	}
+}
